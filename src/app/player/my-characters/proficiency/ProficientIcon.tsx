b668e6c82a12c21e204007c813336312c62121a6
Wayne Leroux <WayneLeroux@gmail.com>
2025-06-30 13:24:32 -0400

    Added profile page to my characters

--- src/app/player/my-characters/proficiency/ProficientIcon.tsx
+++ src/app/player/my-characters/proficiency/ProficientIcon.tsx
@@ -1,8 +1,6 @@
 export const ProficientIcon = () => (
-    <span title="Proficient"><svg width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <circle stroke="white" strokeWidth="1" fill="white"
-                cx="8" cy="8" r="4" />
-        <circle stroke="white" strokeWidth="2" fill="transparent" strokeDasharray="2 2"
-                cx="8" cy="8" r="7" />
-    </svg></span>
+  <span title="Proficient"><svg width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <circle stroke="white" strokeWidth="1" fill="white" cx="8" cy="8" r="4" />
+    <circle stroke="white" strokeWidth="2" fill="transparent" strokeDasharray="2 2" cx="8" cy="8" r="7" />
+  </svg></span>
 );
