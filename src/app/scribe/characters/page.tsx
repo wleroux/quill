c33e4cedaab373b17c0e6b186629189f3839fafb
Wayne Leroux <WayneLeroux@gmail.com>
2025-06-30 22:07:26 -0400

    Added ability for scribes to see all characters

--- src/app/scribe/characters/page.tsx
+++ src/app/scribe/characters/page.tsx
@@ -1,8 +1,8 @@
-import {getCharactersAction} from "@/actions/CharactersActions";
+import {getAllCharactersAction} from "@/actions/CharactersActions";
 import React from "react";
 import {ScribeCharacters} from "@/app/scribe/characters/Characters";
 
 export default async function CharacterPage() {
-  const characters = await getCharactersAction();
+  const characters = await getAllCharactersAction();
   return <ScribeCharacters characters={characters} />
 }
