e77ca9090eb65ac8080e5b30ad4fb378b339fa0d
Wayne Leroux <WayneLeroux@gmail.com>
2025-07-02 17:01:16 -0400

    Allow the reprocessing of characters from audit logs

--- src/app/scribe/characters/validateCharacter.ts
+++ src/app/scribe/characters/validateCharacter.ts
@@ -1,5 +1,5 @@
 import {Character, INITIAL_CHARACTER} from "@/model/character/Character";
-import {Result, ValidResult} from "@/model/processor/Result";
+import {ErrorResult, Result, ValidResult} from "@/model/processor/Result";
 import {ProcessorError} from "@/model/processor/Processor";
 import {startingStatProcessor} from "@/model/character/create/starting-stat/StartingStatProcessor";
 import {DefaultStartingStatChoice} from "@/model/character/create/starting-stat/StartingStatChoice";
@@ -10,21 +10,26 @@ import {DefaultBackgroundChoice} from "@/model/character/create/background/Backg
 import {getProgressChoice, progressProcessor} from "@/model/character/progress/progressProcessor";
 
 export function validateCharacter(initialValue: Character): Result<Character, ProcessorError[]> {
-  let result = ValidResult.of(INITIAL_CHARACTER(initialValue.id, initialValue.ownerID));
-  result = result.flatMap(value => startingStatProcessor(value, DefaultStartingStatChoice, {
-    type: "starting-stat",
-    data: initialValue.startingStat
-  }));
-  result = result.flatMap(value => speciesProcessor(value, DefaultSpeciesChoice, {
-    type: "species",
-    data: initialValue.species
-  }));
-  result = result.flatMap(value => backgroundProcessor(value, DefaultBackgroundChoice, {
-    type: "background",
-    data: initialValue.background
-  }));
-  for (const progress of initialValue.progress) {
-    result.flatMap(value => progressProcessor(value, getProgressChoice(value, progress), progress));
+  try {
+    let result = ValidResult.of(INITIAL_CHARACTER(initialValue.id, initialValue.ownerID));
+    result = result.flatMap(value => startingStatProcessor(value, DefaultStartingStatChoice, {
+      type: "starting-stat",
+      data: initialValue.startingStat
+    }));
+    result = result.flatMap(value => speciesProcessor(value, DefaultSpeciesChoice, {
+      type: "species",
+      data: initialValue.species
+    }));
+    result = result.flatMap(value => backgroundProcessor(value, DefaultBackgroundChoice, {
+      type: "background",
+      data: initialValue.background
+    }));
+    for (const progress of initialValue.progress) {
+      result.flatMap(value => progressProcessor(value, getProgressChoice(value, progress), progress));
+    }
+    return result;
+  } catch (e) {
+    // If an exception, just error out completely!
+    return ErrorResult.of([new ProcessorError("ERROR", [], undefined, undefined)]);
   }
-  return result;
 }
