49d67dba32a547cfc80ff0181c9fbd047a825340
Wayne Leroux <WayneLeroux@gmail.com>
2025-07-03 01:41:01 -0400

    Fixed proficiency icons when light mode

--- src/app/player/my-characters/proficiency/ExpertiseIcon.tsx
+++ src/app/player/my-characters/proficiency/ExpertiseIcon.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 export const ExpertiseIcon = () => (
-  <span title="Expertise"><svg width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <circle stroke="white" strokeWidth="2" fill="white" cx="8" cy="8" r="7" />
+  <span title="Expertise"><svg width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg" className="stroke-black dark:stroke-white">
+    <circle strokeWidth="2" fill="white" cx="8" cy="8" r="7" />
   </svg></span>
 );
--- src/app/player/my-characters/proficiency/ProficientIcon.tsx
+++ src/app/player/my-characters/proficiency/ProficientIcon.tsx
@@ -1,6 +1,6 @@
 export const ProficientIcon = () => (
-  <span title="Proficient"><svg width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <circle stroke="white" strokeWidth="1" fill="white" cx="8" cy="8" r="4" />
-    <circle stroke="white" strokeWidth="2" fill="transparent" strokeDasharray="2 2" cx="8" cy="8" r="7" />
+  <span title="Proficient"><svg width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg" className="stroke-black dark:stroke-white fill-black dark:fill-white">
+    <circle strokeWidth="1" cx="8" cy="8" r="4" />
+    <circle strokeWidth="2" fill="transparent" strokeDasharray="2 2" cx="8" cy="8" r="7" />
   </svg></span>
 );
