284c141ad09391dc342ad717611301e33b2e5d40
Wayne Leroux <WayneLeroux@gmail.com>
2025-07-02 23:53:13 -0400

    Add stat increment value to background attributes

--- src/model/character/create/background/background-attribute/BackgroundAttributeField.tsx
+++ src/model/character/create/background/background-attribute/BackgroundAttributeField.tsx
@@ -7,6 +7,13 @@ import {ATTRIBUTES} from "@/model/source/Attribute";
 import {FieldSet} from "@/lib/components/FieldSet";
 
 export function BackgroundAttributeField({character, choice, value, onChange}:  {character: Character, choice: BackgroundAttributeChoice, value: BackgroundAttributeDecision | undefined, onChange: (value: BackgroundAttributeDecision | undefined) => void }) {
+  const init = character.stats;
+  const attr1 = {...character.stats};
+  if (value?.data.attributeID1) attr1[value?.data.attributeID1] += 1;
+  const attr2 = {...character.stats};
+  if (value?.data.attributeID1) attr2[value?.data.attributeID1] += 1;
+  if (value?.data.attributeID2) attr2[value?.data.attributeID2] += 1;
+
   return (
     <FieldSet inline>
       <DropdownField label="Attribute" value={value?.data.attributeID1} onChange={(ev) => {
@@ -24,7 +31,7 @@ export function BackgroundAttributeField({character, choice, value, onChange}:  
           .filter(attributeID => choice.data.condition === undefined || choice.data.condition(attributeID, character))
           .filter(attributeID => attributeID !== value?.data.attributeID2 || attributeID !== value?.data.attributeID3)
           .map(attributeID => ({
-          label: ATTRIBUTES[attributeID].label,
+          label: `${ATTRIBUTES[attributeID].label} (${init[attributeID]} ⇒ ${init[attributeID] + 1})`,
           value: attributeID,
         }))
       } />
@@ -43,7 +50,7 @@ export function BackgroundAttributeField({character, choice, value, onChange}:  
           .filter(attributeID => choice.data.condition === undefined || choice.data.condition(attributeID, character))
           .filter(attributeID => attributeID !== value?.data.attributeID1 || attributeID !== value?.data.attributeID3)
           .map(attributeID => ({
-          label: ATTRIBUTES[attributeID].label,
+          label:  `${ATTRIBUTES[attributeID].label} (${attr1[attributeID]} ⇒ ${attr1[attributeID] + 1})`,
           value: attributeID,
         }))
       } />
@@ -62,7 +69,7 @@ export function BackgroundAttributeField({character, choice, value, onChange}:  
           .filter(attributeID => choice.data.condition === undefined || choice.data.condition(attributeID, character))
           .filter(attributeID => attributeID !== value?.data.attributeID1 || attributeID !== value?.data.attributeID2)
           .map(attributeID => ({
-          label: ATTRIBUTES[attributeID].label,
+          label: `${ATTRIBUTES[attributeID].label} (${attr2[attributeID]} ⇒ ${attr2[attributeID] + 1})`,
           value: attributeID,
         }))
       } />
