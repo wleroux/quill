8656622c484a9456b714fdf5168348c857c51cef
Wayne Leroux <WayneLeroux@gmail.com>
2025-07-08 10:02:52 -0400

    Remove Gloom Stalker features from Hunter

--- src/model/source/phb/level/ranger/RangerHunter.ts
+++ src/model/source/phb/level/ranger/RangerHunter.ts
@@ -1,22 +1,11 @@
-import {is} from "@/model/source/condition/generic/IsCondition";
 import {ClassID, Level} from "@/model/source/model/Level";
-import {SpellID} from "@/model/source/model/Spell";
 import {PHB_RANGER_3, PHB_RANGER_4} from "@/model/source/phb/level/ranger/RangerBase";
-import {SkillID} from "@/model/source/model/Skill";
 
 const PHB_RANGER_HUNTER_3: Level = {
   replace: "Ranger 2",
   label: "Ranger (Hunter) 3",
   choices: [
-    ...PHB_RANGER_3.choices,
-    {type: "spell", data: {
-      choiceID: "ranger (hunter)::spell-1",
-      condition: is<SpellID>("Disguise Self")
-    }},
-    {type: "skill", data: {
-      choiceID: "ranger (hunter)::skill-1",
-      condition: is<SkillID>("deception", "performance", "persuasion")
-    }}
+    ...PHB_RANGER_3.choices
   ]
 } as const;
 
