259c66331338526500a696ccb04b3881dfea24a3
Wayne Leroux <WayneLeroux@gmail.com>
2025-09-02 17:50:54 -0400

    Prevent issue where no valid skill choice prevents players from leveling up

--- src/model/source/choice/skill/SkillField.tsx
+++ src/model/source/choice/skill/SkillField.tsx
@@ -1,14 +1,13 @@
 import {DropdownField} from "@/lib/components/DropdownField";
 import {SkillDecision} from "@/model/source/choice/skill/SkillDecision";
 import {SkillChoice} from "@/model/source/choice/skill/SkillChoice";
-import {SKILL_IDS, SKILLS} from "@/model/source/Skill";
+import {SKILLS} from "@/model/source/Skill";
 import {Character} from "@/model/character/Character";
 import {useEffect} from "react";
+import {getValidSkills} from "@/model/source/choice/skill/SkillProcessor";
 
 export function SkillField({character, choice, value, onChange}: {character: Character, choice: SkillChoice, value: SkillDecision | undefined, onChange: (value: SkillDecision | undefined) => void}) {
-  const VALID_SKILLS = SKILL_IDS
-    .filter(skillID => character.skills[skillID] === "untrained")
-    .filter(skillID => choice.data.condition === undefined || choice.data.condition(skillID, character));
+  const VALID_SKILLS = getValidSkills(character, choice);
   useEffect(() => {
     if (VALID_SKILLS.length === 1 && VALID_SKILLS[0] !== value?.data.skillID) {
       onChange({type: "skill", data: {skillID: VALID_SKILLS[0]}})
--- src/model/source/choice/skill/SkillProcessor.ts
+++ src/model/source/choice/skill/SkillProcessor.ts
@@ -2,14 +2,25 @@ import {SkillChoice} from "@/model/source/choice/skill/SkillChoice";
 import {SkillDecision} from "@/model/source/choice/skill/SkillDecision";
 import {Processor, ProcessorError} from "@/model/processor/Processor";
 import {ErrorResult, ValidResult} from "@/model/processor/Result";
+import {SKILL_IDS} from "@/model/source/Skill";
+import {Character} from "@/model/character/Character";
+
+export function getValidSkills(value: Character, choice: SkillChoice) {
+  return SKILL_IDS
+    .filter(skillID => value.skills[skillID] === "untrained")
+    .filter(skillID => choice.data.condition === undefined || choice.data.condition(skillID, value));
+}
 
 export const skillProcessor: Processor<SkillChoice, SkillDecision | undefined> = (value, choice, decision) => {
   if (choice.data.enabled && !choice.data.enabled(undefined, value))
     return ValidResult.of(value);
 
   // VALIDATE OPTIONAL
-  if (decision === undefined)
-    return ErrorResult.of([new ProcessorError("MISSING DECISION", [choice.data.choiceID], choice, decision)]);
+  if (decision === undefined) {
+    if (getValidSkills(value, choice).length > 0)
+      return ErrorResult.of([new ProcessorError("MISSING DECISION", [choice.data.choiceID], choice, decision)]);
+    return ValidResult.of(value);
+  }
 
   // VALIDATE SKILL
   if (value.skills[decision.data.skillID] !== "untrained")
